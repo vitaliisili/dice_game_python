fcf5d50cea9846ece3da9d1252c89c2356d07ad3
Vitalii Sili <vitaliisili@yahoo.com>
2023-05-29 18:59:43 +0200

    deploy configuration

--- src/baboo_game/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-    from importlib.metadata import version, PackageNotFoundError
-except ImportError:
-    from importlib_metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("baboo_game")
-except PackageNotFoundError:
-    pass
